952d7e2b83a3a4a16b99df3387b22c0e86f97704
Emmanuel Durand <emmanueldurand@gmail.com>
2014-09-07 18:38:31 -0400

    Improved shmimage.py

--- shmimage.py
+++ shmimage.py
@@ -10,6 +10,7 @@ class ShmImage():
         self.shmreader = None
         self.shmtype = None
         self.image = None
+        self.callback = None
 
         self.bppMath = re.compile('(bpp=\(int\))(\d*)')
         self.widthMatch = re.compile('(width=\(int\))(\d*)')
@@ -25,7 +26,20 @@ class ShmImage():
         array = np.array(tple, dtype=np.uint16)
         array.shape = (height, width)
         self.image = array
+        self.shmtype = datatype
+
+        if self.callback is not None:
+            self.callback(self.image, self.shmtype)
 
     
-    def connect(self, shmpath=""):
+    def connect(self, shmpath="", callback=None):
+        self.callback = callback
         self.shmreader = shm.Reader(path=shmpath, callback=ShmImage.callback, user_data=self)
+
+
+    def getType(self):
+        return self.shmtype
+
+
+    def getImage(self):
+        return self.image
