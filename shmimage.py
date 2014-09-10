b657611d8e2f3f9adbebb1babdf0f51634746217
Emmanuel Durand <emmanueldurand@gmail.com>
2014-09-10 08:39:47 -0400

    Added DetectSurface class, not working yet. Some optimizations

--- shmimage.py
+++ shmimage.py
@@ -22,14 +22,13 @@ class ShmImage():
         height = int(self.heightMatch.search(datatype).group(2))
         sizeStr = str(width * height)
 
-        tple = struct.unpack(sizeStr + "H", image)
-        array = np.array(tple, dtype=np.uint16)
+        array = np.frombuffer(image, np.uint16, width * height)
         array.shape = (height, width)
         self.image = array
         self.shmtype = datatype
 
         if self.callback is not None:
-            self.callback(self.image, self.shmtype)
+            self.callback(self.image)
 
     
     def connect(self, shmpath="", callback=None):
@@ -43,3 +42,43 @@ class ShmImage():
 
     def getImage(self):
         return self.image
+
+
+class DetectSurface():
+    def __init__(self):
+        self.image = None
+        self.detector = None
+        self.points = None
+
+        self.thresholdValue = 127
+
+        self.detectorParams = cv.SimpleBlobDetector_Params()
+        self.detectorParams.filterByColor = False
+        self.detectorParams.filterByConvexity = False
+        self.detectorParams.filterByInertia = False
+
+
+    def setImage(self, img):
+        if self.detector is None:
+            self.detector = cv.SimpleBlobDetector(self.detectorParams)
+
+        self.image = img.astype(np.uint8)
+        self.image = cv.threshold(self.image, self.thresholdValue, 255, cv.THRESH_BINARY)[1]
+        self.points = self.detector.detect(self.image)
+
+
+    def getPoints(self):
+        return self.points
+
+
+    def getMatrix(self):
+        pass
+
+
+def test():
+    shm = ShmImage()
+    shm.connect("/tmp/switcher_nodeserver_posturesrc6_ir")
+
+    detector = DetectSurface()
+
+    return shm, detector
